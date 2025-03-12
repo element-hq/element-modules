32adc2901e1646c97acd4368cadae561a0597c4f
Michael Telatynski <7t3chguy@gmail.com>
2025-03-12 10:33:29 +0000

    Iterate

--- knip.ts
+++ knip.ts
@@ -16,5 +16,12 @@ export default {
         "@matrix-org/react-sdk-module-api",
         // Unlisted peer dependency for @matrix-org/react-sdk-module-api
         "matrix-web-i18n",
+        // Needed to satisfy the peer dependency of @element-hq/element-web-playwright-common
+        "@playwright/test",
+        "@element-hq/element-web-module-api",
+    ],
+    entry: [
+        "packages/element-web-playwright-common/src/testcontainers/*",
+        "packages/element-web-playwright-common/src/fixtures/services.ts",
     ],
 } satisfies KnipConfig;
