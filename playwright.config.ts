d08991ba86776285574db0d2c639bdd2c01ae182
Michael Telatynski <7t3chguy@gmail.com>
2025-03-12 15:12:44 +0000

    Iterate

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,7 +62,7 @@ export default defineConfig<Options>({
     },
     workers: 1,
     retries: process.env.CI ? 2 : 0,
-    reporter: process.env.CI ? [["blob"], ["github"]] : [["html", { outputFolder: "playwright-html-report" }]],
+    reporter: process.env.CI ? [["html"], ["github"]] : [["html", { outputFolder: "playwright-html-report" }]],
     snapshotPathTemplate: "{snapshotDir}/{testFilePath}/{arg}-{platform}{ext}",
     forbidOnly: !!process.env.CI,
 });
