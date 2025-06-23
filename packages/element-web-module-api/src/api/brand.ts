163d240744226884a283a0cc7c241d2ceae164f8
Half-Shot <will@half-shot.uk>
2025-06-23 15:51:51 +0100

    Drop favicon support

--- packages/element-web-module-api/src/api/brand.ts
+++ packages/element-web-module-api/src/api/brand.ts
@@ -3,22 +3,6 @@
  * the state of the application.
  * @public
  */
-export type FaviconRenderOptions = {
-    /**
-     * The number of unread notifications the user has. May be undefined if the notification count isn't known yet (such as during initial loading).
-     */
-    notificationCount?: number;
-    /**
-     * Has an error occured (e.g. failure to sync) and should be represented in the favicon's badge.
-     */
-    errorDidOccur: boolean;
-};
-
-/**
- * Options to be used when rendering a favicon. This represents
- * the state of the application.
- * @public
- */
 export type TitleRenderOptions = {
     /**
      * The number of unread notifications the user has.
@@ -44,15 +28,6 @@ export type TitleRenderOptions = {
 };
 
 /**
- * Function to render the favicon.
- * @public
- * @param opts - Current state of the client.
- * @returns The URL of the badge to be rendered.
- *          If you are rendering to a canvas, you can use a Data URL here.
- */
-export type FaviconRenderFunction = (opts: FaviconRenderOptions) => string;
-
-/**
  * Function to render the window title.
  * @public
  * @param opts - Current state of the client.
@@ -64,15 +39,6 @@ export type TitleRenderFunction = (opts: TitleRenderOptions) => string;
  * @public
  */
 export interface BrandApi {
-    /**
-     * Register a function to render a favicon. This function will be called whenever
-     * the application needs to re-render the icon.
-     * @public
-     * @param renderFunction - The function to use to render.
-     * @throws If another module has registered a favicon renderer, this will throw.
-     */
-    registerFaviconRenderer(renderFunction: FaviconRenderFunction): void;
-
     /**
      * Register a function to render the window title. This function will be called whenever
      * the application needs to re-render the title of the browser window.
