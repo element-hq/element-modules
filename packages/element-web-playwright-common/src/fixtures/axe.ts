6bc858286a870532eae2293cfe3dc0aba96b5430
Richard van der Hoff <richard@matrix.org>
2025-11-26 22:54:06 +0000

    Avoid `mergeTests` in test fixture declarations
    Normally, one can find the documentation on a playwright test fixture by
    finding its declaration (i.e., you can ctrl-click on the fixture name and find
    its documentaion).
    
    However, `mergeTests` re-declares the type, making it much harder to find the
    documentation on a given fixture.
    
    It's easy enough to avoid `mergeTests`: we just structure our `test.extend`
    calls as a strict hieirarchy.

--- packages/element-web-playwright-common/src/fixtures/axe.ts
+++ packages/element-web-playwright-common/src/fixtures/axe.ts
@@ -6,8 +6,9 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { test as base } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
+
+import { test as base } from "./user";
 
 export const test = base.extend<{
     /**
