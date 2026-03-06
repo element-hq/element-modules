44aff2c754e91c80db16cb0062d2ad10d08cae40
David Baker <dbkr@users.noreply.github.com>
2026-03-06 10:28:14 +0000

    Add hacky fix for tooltip context horror

--- modules/widget-toggles/element-web/src/index.tsx
+++ modules/widget-toggles/element-web/src/index.tsx
@@ -6,6 +6,7 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { type JSX } from "react";
+import { TooltipProvider } from "@vector-im/compound-web";
 
 import type { Module, Api, ModuleFactory } from "@element-hq/element-web-module-api";
 import { CONFIG_KEY, WidgetTogglesConfig } from "./config";
@@ -43,7 +44,19 @@ class WidgetToggleModule implements Module {
             }
 
             if (toggleElements.length === 0) return undefined;
-            return <>{toggleElements}</>;
+
+            // XXX: We shouldn't have to add another TooltipProvider here, it should
+            // be using the one in MatrixChat in Element Web, but thanks to the fact
+            // that contexts are "magically" identified by class, it doesn't pick up the
+            // context because we use a different copy of compound-web. We'll probably
+            // need to fix this at some point, possibly by moving compound's tooltip stuff
+            // out to its own mini-module that can be provided at runtime by Element Web,
+            // unless React make contexts more sensible.
+            // Annoyingly this does actually cause the tooltips to behave a bit weirdly:
+            // there's a delay before the tooltip appears when yo move between these buttons
+            // and the rest of the header buttons, whereas moving between the other header
+            // buttons, the tooltips appear straight away once one has appeared.
+            return <TooltipProvider>{toggleElements}</TooltipProvider>;
         });
     }
 }
